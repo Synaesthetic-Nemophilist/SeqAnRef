288b06e0adad3b84809433384a5a553a74803806
nomaterials <nomaterials@gmail.com>
2018-09-03 11:27:24 +0300

    Optimal Search Scheme (Serial & Parallel)

---
+++ Algorithms/Pattern_Matching/Optimal_Search_Scheme/main.cpp
@@ -0,0 +1,54 @@
+//
+// Created by kougianosg on 03-Sep-18.
+//
+
+#include <set>
+#include <mutex>
+
+#include <seqan/index.h>
+
+using namespace seqan;
+
+int main()
+{
+
+    DnaString genome(
+            "GAGAGGCCACTCGCAGGATTAAGTCAATAAGTTAATGGCGTGGGGTTATGGTATGGGGGTTCTCGCCCACAGTGACCTCATCGGT"
+            "GCATTTCCTCATCGTAGGCGGAACGGTAGACACAAGGCATGATGTCAAATCGCGACTCCAATCCCAAGGTCGCAAGCCTATATAG"
+            "GAACCCGCTTATGCCCTCTAATCCCGGACAGACCCCAAATATGGCATAGCTGGTTGGGGGTACCTACTAGGCACAGCCGGAAGCA");
+
+    Index<DnaString, BidirectionalIndex<FMIndex<> > > index(genome);
+
+    // C++ lambda
+    auto delegate = [](auto & iter, DnaString const & needle, uint8_t errors)
+    {
+        for (auto occ : getOccurrences(iter))
+            std::cout << occ << std::endl;
+    };
+
+    DnaString pattern("GGGGTTAT");
+    std::cout << "Hits with up to 2 errors (HammingDistance):" << std::endl;
+    find<0, 2>(delegate, index, pattern, HammingDistance());
+
+    StringSet<DnaString> patterns;
+    appendValue(patterns, "GGGGTTAT");
+    appendValue(patterns, "CTAGCTAA");
+    std::cout << "Hits with 1-2 errors (HammingDistance):" << std::endl;
+    find<1, 2>(delegate, index, patterns, HammingDistance(), Serial());
+
+    // Parallel StringSet search using mutex for concurrency
+    std::mutex mtx;
+    std::set<Pair<DnaString, unsigned> > hits;
+    auto delegateParallel = [&hits, &mtx](auto & iter, DnaString const & needle, uint8_t errors)
+    {
+        std::lock_guard<std::mutex> lck(mtx); // critical section below this line
+        for (auto occ : getOccurrences(iter))
+            hits.insert(Pair<DnaString, unsigned>(needle, occ));
+    };
+    find<2, 3>(delegateParallel, index, patterns, HammingDistance(), Parallel());
+    std::cout << "Hits with 2-3 errors (HammingDistance, no duplicates):" << std::endl;
+    for (const auto &hit : hits)
+        std::cout << hit << std::endl;
+
+    return 0;
+}
